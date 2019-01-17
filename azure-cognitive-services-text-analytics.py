dbccf36b98fcffc80363f66afa6a2ee5ed3faad8
Mike Stiers <mike.stiers@gmail.com>
2019-01-17 18:59:31 -0500

    working hello world

--- azure-cognitive-services-text-analytics.py
+++ azure-cognitive-services-text-analytics.py
@@ -1,8 +1,14 @@
-
+subscription_key = ""
 assert subscription_key
-text_analytics_base_url = "https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/"
+text_analytics_base_url = "https://canadacentral.api.cognitive.microsoft.com/text/analytics/v2.0/"
 language_api_url = text_analytics_base_url + "languages"
 print(language_api_url)
+
+documents = { 'documents': [
+    { 'id': '1', 'text': 'This is a document written in English.' },
+    { 'id': '2', 'text': 'Este es un document escrito en Español.' },
+    { 'id': '3', 'text': '这是一个用中文写的文件' }
+]}
 
 import requests
 from pprint import pprint
