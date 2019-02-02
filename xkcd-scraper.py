b4e795dc431fe7409be5884a1f5c77b43a8d87cf
Mike Stiers <mike.stiers@gmail.com>
2019-02-01 19:27:22 -0500

    loop through first 15 test

--- xkcd-scraper.py
+++ xkcd-scraper.py
@@ -2,14 +2,16 @@ import requests, os
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-xkcdUrl = 'http://xkcd.com'
-xkcdPage = urlopen(xkcdUrl)
-
-
-soup = BeautifulSoup(xkcdPage, 'html.parser')
-comicElement = soup.find("div", {"id": "comic" })
-print(comicElement)
-comicImage = 'http:' + comicElement.find('img').attrs['src']
+count = 1
+while (count < 15):
+    xkcdUrl = 'http://xkcd.com/'+str(count)
+    xkcdPage = urlopen(xkcdUrl)
+    count += 1
+    soup = BeautifulSoup(xkcdPage, 'html.parser')
+    comicElement = soup.find("div", {"id": "comic" })
+    #print(comicElement)
+    comicImage = 'http:' + comicElement.find('img').attrs['src']
+    print(comicImage)
 
 
 #images = soup.findAll('img')
