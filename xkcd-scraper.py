d79053ea932e9f235f7edbb4e17793b4fe7007cd
Mike Stiers <mike.stiers@gmail.com>
2019-02-02 19:25:56 -0500

    get all comic images from current to 1

--- xkcd-scraper.py
+++ xkcd-scraper.py
@@ -2,17 +2,20 @@ import requests, os
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-count = 1
-while (count < 15):
+xkcdUrl = 'http://xkcd.com/'
+xkcdPage = urlopen(xkcdUrl)
+soup = BeautifulSoup(xkcdPage, 'html.parser')
+previousComic = ((soup.find("a", {"rel":"prev"})).attrs['href']).strip('/')
+currentCommic = int(previousComic) + 1
+
+count = currentCommic
+while (count > 2100):
     xkcdUrl = 'http://xkcd.com/'+str(count)
     xkcdPage = urlopen(xkcdUrl)
-    count += 1
+    count -= 1
     soup = BeautifulSoup(xkcdPage, 'html.parser')
     comicElement = soup.find("div", {"id": "comic" })
-    #print(comicElement)
     comicImage = 'http:' + comicElement.find('img').attrs['src']
     print(comicImage)
 
-
-#images = soup.findAll('img')
 print()
