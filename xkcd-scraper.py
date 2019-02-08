8cb2ba7451dea00b3a31e6d70868182564de147c
Mike Stiers <mike.stiers@gmail.com>
2019-02-07 19:49:08 -0500

    check to see if latest comic is newer than last logged

--- xkcd-scraper.py
+++ xkcd-scraper.py
@@ -16,19 +16,20 @@ soup = BeautifulSoup(xkcdPage, 'html.parser')
 previousComic = ((soup.find("a", {"rel":"prev"})).attrs['href']).strip('/')
 currentCommic = int(previousComic) + 1
 
-count = currentCommic
-while (count > 0):
-    xkcdUrl = 'http://xkcd.com/'+str(count)
-    xkcdPage = urlopen(xkcdUrl)
-    count -= 1
-    soup = BeautifulSoup(xkcdPage, 'html.parser')
-    comicElement = soup.find("div", {"id": "comic" })
-    comicImage = 'http:' + comicElement.find('img').attrs['src']
-    print(comicImage)
-    c.execute("INSERT OR IGNORE INTO comics VALUES (?, ?);", (count, comicImage))
-    conn.commit()
-    #if (count % 5 == 0):
-        #time.sleep(10)
+if (currentCommic >= lastLoggedComic):
+    count = currentCommic
+    while (count > lastLoggedComic):
+        xkcdUrl = 'http://xkcd.com/'+str(count)
+        xkcdPage = urlopen(xkcdUrl)
+        count -= 1
+        soup = BeautifulSoup(xkcdPage, 'html.parser')
+        comicElement = soup.find("div", {"id": "comic" })
+        comicImage = 'http:' + comicElement.find('img').attrs['src']
+        print(comicImage)
+        c.execute("INSERT OR IGNORE INTO comics VALUES (?, ?);", (count, comicImage))
+        conn.commit()
+        #if (count % 5 == 0):
+            #time.sleep(10)
 
 conn.close()
 
