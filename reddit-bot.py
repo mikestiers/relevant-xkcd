fa5f2e0ba53459b1951f8f027ac1e606316e17c1
Mike Stiers <mike.stiers@gmail.com>
2019-02-22 17:50:08 -0500

    working on matching phrases

--- reddit-bot.py
+++ reddit-bot.py
@@ -19,18 +19,23 @@ def checkSentiment(title, selfText):
     redditResponse  = requests.post(language_api_url, headers=headers, json=documents)
     redditResponseJson = json.loads(redditResponse.content)
     languages = redditResponse.json()
-    pprint(languages)
+    #pprint(languages)
     searchHeaders = {"content-type": "application/json", "api-key": "26B2F4C6F9DD073A31EEBD94E870F1AF"}
     searchResponse = requests.get(search_query_base_url + "magnetic", headers=searchHeaders)
     searchResponseJson = json.loads(searchResponse.text)
 
     # dictionary comprehension - look into it 
     # https://superuser.com/questions/1005263/python-script-to-compare-the-keys-of-2-dictionaries-and-if-equal-print-value-of
-    matchingKeys = {key:documents[key] for key in searchResponseJson if key in documents}
+    #matchingKeys = {key:documents[key] for key in searchResponseJson if key in documents}
 
-    for keyphrase in searchResponseJson['value'][0]['keyphrases']:
-        print(keyphrase)
+
+    for searchResponseKeyphrase in searchResponseJson['value'][0]['keyphrases']:
+        for redditResponseKeyPhrase in redditResponseJson['documents'][0]['keyPhrases']:
+            if searchResponseKeyphrase != redditResponseKeyPhrase:
+                print(searchResponseKeyphrase)
+        return
     return
+
 
 
 # reddit bot praw magic
